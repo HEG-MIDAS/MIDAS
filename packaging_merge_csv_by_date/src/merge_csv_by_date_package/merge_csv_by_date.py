20b92e76c2bdb0e392e89863d1ac1ea4ca75041a
David Nogueiras <david.nogueiras@outlook.com>
2022-03-09 17:27:50 +0100

    fix bug when merging files

--- packaging_merge_csv_by_date/src/merge_csv_by_date_package/merge_csv_by_date.py
+++ packaging_merge_csv_by_date/src/merge_csv_by_date_package/merge_csv_by_date.py
@@ -109,37 +109,46 @@ def merge_csv_by_date(old_data_file_path: str, new_data_file_path: str) -> str:
             pos_old = 0
             pos_new = 0
 
-            while pos_old != len(old_data_file_array) and pos_new != len(new_data_file_array):
-                new_splitted_line = re.split('[,;]', new_data_file_array[pos_new])
-                old_splitted_line = re.split('[,;]', old_data_file_array[pos_old])
+            new_stopped = False
+            old_stopped = False
 
-                if is_string_date(new_splitted_line[0]) and is_string_date(old_splitted_line[0]):
-                    if string_to_date(new_splitted_line[0]) > string_to_date(old_splitted_line[0]):
+            while pos_old != len(old_data_file_array) or pos_new != len(new_data_file_array):
+
+                if pos_old == len(old_data_file_array):
+                    old_stopped = True
+                if pos_new == len(new_data_file_array):
+                    new_stopped = True
+
+                new_splitted_line = [''] if new_stopped else re.split('[,;]', new_data_file_array[pos_new])
+                old_splitted_line = [''] if old_stopped else re.split('[,;]', old_data_file_array[pos_old])
+                
+                # Check that the first element of new or old splitted line is a date or that one has stopped and the one is a date
+                if (is_string_date(new_splitted_line[0]) and is_string_date(old_splitted_line[0])) or (is_string_date(new_splitted_line[0]) and old_stopped) or (new_stopped and is_string_date(old_splitted_line[0])):
+                    if (not old_stopped) and ((new_stopped and is_string_date(old_splitted_line[0])) or (string_to_date(new_splitted_line[0]) > string_to_date(old_splitted_line[0]))):
                         merged_data_file_array.append(old_data_file_array[pos_old])
                         pos_old += 1
+                    elif (not new_stopped) and ((is_string_date(new_splitted_line[0]) and old_stopped) or (string_to_date(new_splitted_line[0]) < string_to_date(old_splitted_line[0]))):
+                        merged_data_file_array.append(new_data_file_array[pos_new])
+                        pos_new += 1
                     elif string_to_date(new_splitted_line[0]) == string_to_date(old_splitted_line[0]):
                         merged_data_file_array.append(new_data_file_array[pos_new])
                         pos_new += 1
                         pos_old += 1
-                    else:
-                        merged_data_file_array.append(new_data_file_array[pos_new])
-                        pos_new += 1
                 else:
-                    # Move index until it reaches the first line with a date at position 0
-                    if not is_string_date(new_splitted_line[0]):
+                    # Move indexes until it reaches the first line with a date at position 0
+
+                    if (not is_string_date(new_splitted_line[0])) and not new_stopped:
                         # Write comments and header of the new file
                         merged_data_file_array.append(new_data_file_array[pos_new])
                         pos_new += 1
-                    # Move index until it reaches the first line with a date at position 0
-                    if not is_string_date(old_splitted_line[0]):
+                    if (not is_string_date(old_splitted_line[0])) and not old_stopped:
                         pos_old += 1
-                    
 
+                    
             merged_data_file = open(old_data_file_path, 'w')
             for line in merged_data_file_array:
                 merged_data_file.write("{}\n".format(line))
             merged_data_file.close()
-            #os.remove(new_data_file_path)
             return old_data_file_path
 
     elif os.path.isfile(new_data_file_path):
