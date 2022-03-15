80efe2740feecdc44b4ab6a6d82744dd7582fb30
David Nogueiras <david.nogueiras@outlook.com>
2022-03-15 16:44:18 +0100

    Process data from request and merge into transformed file

--- Climacity/climacity.py
+++ Climacity/climacity.py
@@ -1,7 +1,10 @@
+from ctypes import Array
 import requests
 import time
+import datetime
 import re
 import os
+import numpy as np
 from merge_csv_by_date_package import merge_csv_by_date
 
 
@@ -29,27 +32,56 @@ def decode_request(r: requests.Request) -> None:
 
 
 
-def extract_relevant_data(r: requests.Request) -> None:
+def extract_relevant_data(r: requests.Request) -> Array:
     header_beggining = False
     data_beggining = False
+    array_data = []
     dict_header = {}
     for line in r.iter_lines():
         if data_beggining:
             infos_line = line.decode().split(',')
             if infos_line[1] != '':
-                print(line.decode())
+                array_data.append(line.decode())
         elif header_beggining:
             data_beggining = True
             infos_line = line.decode().split(',')
-            for i,info in enumerate(infos_line):
-                dict_header[info] = i
-            print(dict_header)
+            array_data.append(infos_line)
         elif re.match("^-+-$", line.decode()):
             header_beggining = True
 
+    return array_data
 
-def process_data(data):
-    pass
+
+def process_data(array_data: Array) -> Array:
+    array_processed_data = []
+    array_processed_data.append(array_data[0])
+    array_hour_data = []
+    hour_working = datetime.datetime.now()
+    hour_working_until = datetime.datetime.now()
+    time_set = False
+    for i in range(1, len(array_data)):
+        line_data = array_data[i].split(',')
+        if merge_csv_by_date.string_to_date(line_data[0]) >= hour_working_until:
+            casted_data = np.array([[float(k) if k != '' else np.nan for k in j[2:]] for j in array_hour_data])
+            array_mean_full = [str(hour_working), str(hour_working_until)]
+            array_processed_data.append(array_mean_full + ([str(j) if j != np.nan else '' for j in np.nanmean(casted_data, axis=0)]))
+            array_hour_data = []
+            time_set = False
+
+        if not time_set:
+            hour_working = merge_csv_by_date.string_to_date(line_data[0])
+            hour_working_until = (hour_working+datetime.timedelta(hours=1)).replace(microsecond=0, second=0, minute=0)
+            time_set = True
+        array_hour_data.append(line_data)
+        
+    return array_processed_data
+
+
+def write_array_in_tmp_file(array_data: Array, path_tmp_file: str) -> None:
+    f = open(path_tmp_file, 'w')
+    for line in array_data:
+        f.write("{}\n".format(','.join(line)))
+    f.close()
 
 
 def write_request_in_tmp_file(r: requests.Request, path_tmp_file: str) -> None:
@@ -59,12 +91,12 @@ def write_request_in_tmp_file(r: requests.Request, path_tmp_file: str) -> None:
     f.close()
 
 
+
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 path_tmp_file = '{}/tmp_data_request.csv'.format(__location__)
 path_original_data_file = '{}/../media/original/Climacity/climacity_original_merged.csv'.format(__location__)
-
-today = time.strftime("%Y-%m-%d %H:%M:%S")
+path_transformed_data_file = '{}/../media/transformed/Climacity/climacity_transformed_merged.csv'.format(__location__)
 
 start_date = "2021-02-18"
 end_date = "2021-02-18"
@@ -73,12 +105,30 @@ url = """http://www.climacity.org/Axis/a_data_export.gwt?fdate={}&tdate={}&h_loc
     h_Tsv=on&h_Gh_Avg=on&h_Dh_Avg=on&h_Tamb_Avg=on&h_HRamb_Avg=on&h_Prec_Tot=on&h_Vv_Avg=on&h_Vv_Avg=on&
     h_Vv_Max=on&h_Dv_Avg=on&h_Baro=on&h_CS125_Vis=on&chB_PM25=on&h_PM10=on&h_Hc=on&h_Az=on""".format(start_date, end_date)
 
-# Create a temporary file containing the data requested to climacity (original data)
-write_request_in_tmp_file(download_data(url), path_tmp_file)
-start_time = time.time()
-# Merge temporary file with the file containing all the original data
-merge_csv_by_date(path_original_data_file, path_tmp_file)
-print(time.time()-start_time)
-# Remove the temporary file created
-#os.remove(path_tmp_file)
+today = time.strftime("%Y-%m-%d %H:%M:%S")
 
+print("--------------- Requesting data Climacity : {} ---------------".format(today))
+
+# Request data to download
+r = download_data(url)
+start_time = time.time()
+# Create a temporary file containing the data requested to climacity (original data)
+write_request_in_tmp_file(r, path_tmp_file)
+# Merge temporary file with the file containing all the original data
+print("--------------- Merging new data --------------")
+merge_csv_by_date.merge_csv_by_date(path_original_data_file, path_tmp_file)
+# Remove the temporary file created
+os.remove(path_tmp_file)
+
+print("-------------- Processing data --------------")
+
+array_data = extract_relevant_data(r)
+array_data = process_data(array_data)
+write_array_in_tmp_file(array_data, path_tmp_file)
+
+print("-------------- Merging processed data --------------")
+merge_csv_by_date.merge_csv_by_date(path_transformed_data_file, path_tmp_file)
+os.remove(path_tmp_file)
+print(time.time()-start_time)
+
+print("--------------- Requesting data Climacity ended : {} ---------------".format(today))
