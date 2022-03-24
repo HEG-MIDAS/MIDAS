503fe51f5e4dd54ba5594d3e7128ecfa9dd5f7c7
Amir Alwash <amir@alwash.ch>
2022-03-24 16:57:46 +0100

    Add render html with base layout

--- MIDAS/MIDAS_app/views.py
+++ MIDAS/MIDAS_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there, my name's MIDAS!")
+    return render(request, 'index.html')
