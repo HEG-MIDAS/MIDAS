a9d9e4dfc5852e6378a8c4095191911351265a27
Amir Alwash <amir@alwash.ch>
2022-03-26 01:56:32 +0100

    Separate settings

--- MIDAS/MIDAS/settings/base.py
+++ MIDAS/MIDAS/settings/base.py
@@ -9,13 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -124,3 +122,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'../media')
---
+++ MIDAS/MIDAS/settings/prod.py
@@ -0,0 +1,5 @@
+from .base import *
+import os
+
+DEBUG=False
+STATIC_ROOT=os.path.join(BASE_DIR,'../static')
