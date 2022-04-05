9e07152ed3a0bc090f882279e876e03e840303d9
David Nogueiras <david.nogueiras@outlook.com>
2022-04-05 11:23:43 +0200

    Add templates to change the password and urls + add another way to representated up api

--- MIDAS/MIDAS_app/urls.py
+++ MIDAS/MIDAS_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
 
 urlpatterns=[
     path('', views.index, name='index'),
     path('manage-data/', views.manage_data, name='manage_data'),
     path('harvest-data/', views.harvest_data, name='harvest_data'),
     path('status/<slug:source>.svg', views.status, name='status'),
+    path('change-password/', PasswordChangeView.as_view(template_name='registration/change_password.html'), name='password_change'),
+    path('change-password-success/', PasswordChangeDoneView.as_view(template_name='registration/change_password_success.html'), name='password_change_done'),
 ]
