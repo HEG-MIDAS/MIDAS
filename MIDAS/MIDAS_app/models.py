0a344f01aa3e01e0c7174e734bbc180b5d3dc79d
David Nogueiras <david.nogueiras@outlook.com>
2022-04-08 09:20:58 +0200

    Begin to do model and change name of files climacit

--- MIDAS/MIDAS_app/models.py
+++ MIDAS/MIDAS_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    user = models.CharField(max_length=255)
+    password = models.CharField(max_length=50)
+    email = models.EmailField('Adresse mail', unique=True)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
