7c1371f1bc8467d05c1f4425d09bdb39abd9959f
David Nogueiras <david.nogueiras@outlook.com>
2022-05-03 17:51:11 +0200

    Add update parameters that read the description of the parameters in the original files and update it

---
+++ MIDAS/MIDAS_app/management/commands/update_db_params_infos.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from MIDAS_app.update_db import update_infos_parameters
+
+
+class Command(BaseCommand):
+    help = 'Create the sources, stations, or parameters in DB if they don\'t already exist'
+
+    def handle(self, *args, **options):
+        update_infos_parameters()
--- MIDAS/MIDAS_app/update_db.py
+++ MIDAS/MIDAS_app/update_db.py
@@ -1,10 +1,19 @@
+from turtle import update
 from .models import Source, Station, Parameter, ParametersOfStation
 from django.conf import settings
 import os
+import re
+
+
+header_dictionary = {
+    'PM25': 'PM2.5',
+    'CS125_Vis': 'Dvis_Avg',
+    'Baro': 'Pa_Avg',
+}
 
 
 def insert_sources():
-    print("updating sources")
+    print("Inserting sources")
     sources_csv_path = os.path.join(settings.BASE_DIR, '../sources.csv')
     if os.path.isfile(sources_csv_path):
         sources_csv = open(sources_csv_path, 'r')
@@ -19,7 +28,7 @@ def insert_sources():
 
 
 def insert_stations():
-    print("updating stations")
+    print("Inserting stations")
     station_transformed_path = os.path.join(settings.MEDIA_ROOT, 'transformed/')
     sources_dir = [source.name for source in Source.objects.all()]
     for dir in sources_dir:
@@ -34,7 +43,7 @@ def insert_stations():
 
 
 def insert_parameters():
-    print("updating parameters")
+    print("Inserting parameters")
     station_transformed_path = os.path.join(settings.MEDIA_ROOT, 'transformed/')
     sources_dir = [source.name for source in Source.objects.all()]
     for dir in sources_dir:
@@ -56,5 +65,52 @@ def insert_parameters():
                                 if len(Station.objects.filter(name=station)) > 0 :
                                     p = ParametersOfStation(station=Station.objects.get(name=station), parameter=Parameter.objects.get(name=param))
                                     p.save()
+
+
+def update_infos_parameters():
+    print("Updating parameters")
+    station_original_path = os.path.join(settings.MEDIA_ROOT, 'original/')
+    # Get all the sources in the DB (Directories should be called the same)
+    sources_dir = [source.name for source in Source.objects.all()]
+    # Get all the paramaters and do a list to work with
+    parameters = [parameter.name for parameter in Parameter.objects.all()]
+    # Check that we have at least one parameter
+    if len(parameters) > 0:
+        for dir in sources_dir:
+            working_path = os.path.join(station_original_path, dir)
+            if os.path.isdir(working_path):
+                if dir == 'Climacity':
+                    for file in os.listdir(working_path):
+                        if file.split('.')[-1] == 'csv':
+                            with open(os.path.join(working_path, file)) as f:
+                                for line in f:
+                                    line_array = re.split('\s{3,}', line.rstrip())
+                                    if re.match("^-+-$", line.rstrip()):
+                                        break
+                                    if line_array[0] in parameters:
+                                        Parameter.objects.filter(name=line_array[0]).update(infos=line_array[1])
+                                        parameters.remove(line_array[0])
+                                    if line_array[0] in header_dictionary.keys():
+                                        Parameter.objects.filter(name=header_dictionary[line_array[0]]).update(infos=line_array[1])
+                                        parameters.remove(header_dictionary[line_array[0]])
+                        if len(parameters) <= 0:
+                            break
+                if dir == 'SABRA':
+                    for file in os.listdir(working_path):
+                        if file.split('.')[-1] == 'csv':
+                            with open(os.path.join(working_path, file)) as f:
+                                for line in f:
+                                    if ' Polluant:  ' in line:
+                                        polluant = line.rstrip().split(' Polluant:  ')[1]
+                                        if polluant.split('(')[1][:-1] in parameters:
+                                            Parameter.objects.filter(name=polluant.split('(')[1][:-1]).update(infos=polluant.split('(')[0].replace('  ', ' '))
+                                            parameters.remove(polluant.split('(')[1][:-1])
+                                    elif line == '\n':
+                                        break
+                        if len(parameters) <= 0:
+                            break
+            if len(parameters) <= 0:
+                break
+
 
 # regex detecting 3 or more spaces : \s{3,}
