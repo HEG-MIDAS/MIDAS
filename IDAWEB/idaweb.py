59e81dac86afbc0b6d69a481a8659142daa48e28
Amir Alwash <amir.alwash@hesge.ch>
2022-05-17 18:31:05 +0200

    work on pdf to csv

--- IDAWEB/idaweb.py
+++ IDAWEB/idaweb.py
@@ -3,10 +3,15 @@ import re
 import sys
 import getopt
 import numpy as np
+import tabula.io
 from merge_csv_by_date_package import merge_csv_by_date
 
 def main(argv):
-    print('I EXIST')
+    # Read a PDF File
+    df = tabula.io.read_pdf("/home/amir/Git/MIDAS/IDAWEB/inventory.pdf", pages='all')[0]
+    # convert PDF into CSV
+    tabula.io.convert_into("/home/amir/Git/MIDAS/IDAWEB/inventory.pdf", "/home/amir/Git/MIDAS/IDAWEB/inventory.csv", output_format="csv", pages='all')
+    print(df)
 
 
 if __name__ == "__main__":
