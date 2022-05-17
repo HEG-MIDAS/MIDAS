8405c513d047010f08168944302d5569711d7930
Amir Alwash <amir.alwash@hesge.ch>
2022-05-17 15:56:19 +0200

    Creating IDAWEB folders and files

---
+++ IDAWEB/idaweb.py
@@ -0,0 +1,13 @@
+import os
+import re
+import sys
+import getopt
+import numpy as np
+from merge_csv_by_date_package import merge_csv_by_date
+
+def main(argv):
+    print('I EXIST')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
