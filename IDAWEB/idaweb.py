6d227f987a1ad84bcc40b5fa8d9d2c062440ece5
Amir Alwash <amir@alwash.ch>
2022-05-20 16:41:06 +0200

    adv.

--- IDAWEB/idaweb.py
+++ IDAWEB/idaweb.py
@@ -9,6 +9,11 @@ from merge_csv_by_date_package import merge_csv_by_date
 ## Path of Scraper
 scraper_path = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
+## Root of Project
+root_path = os.path.join(scraper_path,'..')
+## Path of Media
+original_media_path = os.path.join(root_path,'media/original/IDAWEB')
+transformed_media_path = os.path.join(root_path,'media/transformed/IDAWEB')
 
 def createHeaders():
     """Take the inventory csv file and create a headers file from it
@@ -73,9 +78,22 @@ def createInventoryCSV():
     print("Created inventory csv file !")
     createHeaders()
 
+def buildDataFromOrder():
+    return {}
+def fileToData(lines:dict):
+    if(len(lines)<1):
+        return None
+
+    print(lines)
+    return {}
+
 def orderManipulation():
     headerFile = None
-    headers = {}
+    header = {}
+    content = {}
+    stations = []
+
+    # Check and load header file
     if os.path.exists(os.path.join(scraper_path,'headers.csv')) == False:
         print("The headers file wasn't found. run the command with the -i option to generate it.")
         sys.exit(1)
@@ -84,19 +102,52 @@ def orderManipulation():
         for line in headerFile:
             line = line.strip()
             splitted_line = line.split(";",1)
-            headers[splitted_line[0]]=splitted_line[1]
+            header[splitted_line[0]]=splitted_line[1]
+            content[splitted_line[0]] = {}
+            stations.append(splitted_line[0])
+            for splitter_content in splitted_line[1].split(";"):
+                content[splitted_line[0]][splitter_content] = {}
         headerFile.close()
 
+    # Check Order files
     order_files = list(filter(lambda f: f.startswith('order_'),os.listdir(scraper_path)))
+    order_data_files = list(filter(lambda f: f.startswith('order_') and f.endswith('data.txt'),os.listdir(scraper_path)))
+    order_legend_files = list(filter(lambda f: f.startswith('order_') and f.endswith('legend.txt'),os.listdir(scraper_path)))
+    order_files_by_id = {}
     if(len(order_files) == 0):
         print("No order file not found !")
         sys.exit(1)
     elif(len(order_files)%2!=0):
-        print("Some file seems to be missing !\nMake sure you have one order_data and one order_legend for each cod number")
+        print("Some file seems to be missing !\nMake sure you have one order_data and one order_legend for each code number")
+        sys.exit(1)
+    elif len(order_data_files) != len(order_legend_files):
+        print("There isn't the same number of data and legend file.\nPlease make sure to have the corresponding files for each order code !")
         sys.exit(1)
 
-    print(headers)
+    for file in order_files:
+        splitted_name = file.split("_")
+        print(splitted_name[1])
+        if splitted_name[1] not in order_files_by_id:
+            order_files_by_id[splitted_name[1]] = {}
 
+        order_files_by_id[splitted_name[1]][splitted_name[2].replace(".txt","")] = file
+
+    print(stations)
+    for station in stations:
+        station_name = station.replace(" / ","-")
+        station_name = station_name.replace(" /","-")
+        station_file = None
+        try:
+            station_file = open(os.path.join(transformed_media_path,station_name+".csv"),"r")
+        except:
+            station_file = open(os.path.join(transformed_media_path,station_name+".csv"),"x+")
+        currentData = fileToData(station_file.readlines())
+        station_file.close()
+        if currentData == None:
+            print("NOOOOOOOOOOOOO")
+            # Use headers files to generate datas for the station...
+
+    print(order_files_by_id)
 def main(argv):
     try:
       opts, args = getopt.getopt(argv,"ihs")
