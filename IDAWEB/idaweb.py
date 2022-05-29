4223b8619b71631b3dade281e50b34ee1b2c66ef
Amir Alwash <amir@alwash.ch>
2022-05-29 12:47:53 +0200

    -

--- IDAWEB/idaweb.py
+++ IDAWEB/idaweb.py
@@ -83,7 +83,7 @@ def createInventoryCSV():
     print("Created inventory csv file !")
     createHeaders()
 
-def dataToFile(dataset, header):
+def dataToFile(dataset: dict, header:dict) -> int:
     try:
         for station, timestamps in dataset.items():
             station_file = open(os.path.join(transformed_media_path,station)+'.csv',"w+")
@@ -128,7 +128,7 @@ def loadHeader():
 
     return header, content, stations
 
-def orderManipulation():
+def orderManipulation() -> int:
 
     # Load Header
     header, content, stations = loadHeader()
@@ -188,144 +188,24 @@ def orderManipulation():
 
                     # Ignore header line
                     if measures[0] != "time":
-                        if measures[0] not in dataset[station_abbr[station_name]]:
-                            dataset[station_abbr[station_name]][measures[0]] = {}
-                            for param in header[station_abbr[station_name]].split(';'):
-                                dataset[station_abbr[station_name]][measures[0]][param] = ''
+                        # NEED TO CHECK TIMESTAMP TO PARSE IT CORRECTLY (DAILY VALUE!)
+                        try:
+                            timestamp = datetime.datetime.strptime(measures[0],'%Y%m%d%H')
+                            sys.exit(0)
+                            if measures[0] not in dataset[station_abbr[station_name]]:
+                                dataset[station_abbr[station_name]][measures[0]] = {}
+                                for param in header[station_abbr[station_name]].split(';'):
+                                    dataset[station_abbr[station_name]][measures[0]][param] = ''
 
-                        # NEED TO CHECK TIMESTAMP TO PARSE IT CORRECTLY (DAIYL VALUE!)
-                        dataset[station_abbr[station_name]][measures[0]][parameter] = measures[1]
+                            dataset[station_abbr[station_name]][measures[0]][parameter] = measures[1]
+                        except:
+                            print("No Matching Timestamp Found")
+                            return 1
 
             order_station_file.close()
 
     return dataToFile(dataset,header)
-    # # Check Order files
-    # order_files = list(filter(lambda f: f.startswith('order_'),os.listdir(scraper_path)))
-    # order_data_files = list(filter(lambda f: f.startswith('order_') and f.endswith('data.txt'),os.listdir(scraper_path)))
-    # order_legend_files = list(filter(lambda f: f.startswith('order_') and f.endswith('legend.txt'),os.listdir(scraper_path)))
-    # order_files_by_id = {}
-    # if(len(order_files) == 0):
-    #     print("No order file found !")
-    #     sys.exit(1)
-    # elif(len(order_files)%2!=0):
-    #     print("Some file seems to be missing !\nMake sure you have one order_data and one order_legend for each code number")
-    #     sys.exit(1)
-    # elif len(order_data_files) != len(order_legend_files):
-    #     print("There isn't the same number of data and legend file.\nPlease make sure to have the corresponding files for each order code !")
-    #     sys.exit(1)
-    #
-    # for file in order_files:
-    #     splitted_name = file.split("_")
-    #     if splitted_name[1] not in order_files_by_id:
-    #         order_files_by_id[splitted_name[1]] = {}
-    #
-    #     order_files_by_id[splitted_name[1]][splitted_name[2].replace(".txt","")] = file
-    #
-    # stationManipulation = False
-    # for key,value in order_files_by_id.items():
-    #     legend_order_file = open(os.path.join(scraper_path,value["legend"]),'rb')
-    #     stationDataset = {}
-    #     for line in legend_order_file:
-    #         firstLine = False
-    #         decodedLine = line.decode("Windows-1252").strip()
-    #         if("stn       Nom                                  Parameter        Source de données                                  Longitude/Latitude       Coordonnées [km] Altitude [m]" in line.decode("Windows-1252")):
-    #             stationManipulation = True
-    #             firstLine = True
-    #         if("Paramètre" in line.decode("Windows-1252")):
-    #             stationManipulation = False
-    #         if(stationManipulation and not firstLine):
-    #             if line.decode("Windows-1252").strip() != "":
-    #                 decodedLine = decodedLine.replace(" / ","/").replace(" /","/")
-    #                 splittedStation = re.split(r'\s+',decodedLine)
-    #                 if splittedStation[0] not in stationDataset:
-    #                     stationDataset[splittedStation[0]] = {"parameters":[]}
-    #
-    #                 if splittedStation[1] not in stationDataset[splittedStation[0]]:
-    #                     stationDataset[splittedStation[0]]["name"] = splittedStation[1]
-    #
-    #                 if splittedStation[2] not in stationDataset[splittedStation[0]]["parameters"]:
-    #                     stationDataset[splittedStation[0]]["parameters"].append(splittedStation[2])
-    #
-    #                 if splittedStation[3] not in stationDataset[splittedStation[0]]:
-    #                     stationDataset[splittedStation[0]]["source"] = splittedStation[3]
-    #
-    #                 if splittedStation[4] not in stationDataset[splittedStation[0]]:
-    #                     stationDataset[splittedStation[0]]["latlong"] = splittedStation[4]
-    #
-    #                 if splittedStation[5] not in stationDataset[splittedStation[0]]:
-    #                     stationDataset[splittedStation[0]]["coord"] = splittedStation[5]
-    #
-    #                 if splittedStation[6] not in stationDataset[splittedStation[0]]:
-    #                     stationDataset[splittedStation[0]]["altitude"] = splittedStation[6]
-    #     legend_order_file.close()
-    # currentData = None
-    # # print(stations)
-    # for station in stations:
-    #     station_name = station.replace("/","\\")
-    #     station_file = None
-    #     print(station_name)
-    #     try:
-    #         station_file = open(os.path.join(transformed_media_path,station_name+".csv"),"r+")
-    #     except:
-    #         station_file = open(os.path.join(transformed_media_path,station_name+".csv"),"x+")
-    #     # NEED TO COMPLETE THIS FUNCTION !
-    #     currentData = fileToData(station_file.readlines())
-    #     station_file.close()
-    #
-    # if currentData == None:
-    #     currentData = content
-    #
-    #     # DATA ARE LOADED, NOW TO APPLY ORDER TO IT AND REWRITE IT
-    #     # station_file = open(os.path.join(transformed_media_path,station_name+".csv"),"w")
-    #     # station_file.write(json.dumps(currentData))
-    #     # station_file.close()
-    # # print(order_files_by_id)
-    # dataToSave = {}
-    # for key,value in order_files_by_id.items():
-    #     data_order_file = open(os.path.join(scraper_path,value["data"]),'rb')
-    #     orderHeader = None
-    #     for line in data_order_file:
-    #         strippedLine = line.decode("Windows-1252").strip()
-    #         if strippedLine != "":
-    #             if ";" in strippedLine:
-    #                 if strippedLine.startswith("stn"):
-    #                     orderHeader = strippedLine.split(";")
-    #                 else:
-    #                     if orderHeader is not None:
-    #                         strippedLineList = strippedLine.split(";")
-    #                         stationName = stationDataset[strippedLineList[0]]["name"]
-    #                         if stationName not in dataToSave:
-    #                             dataToSave[stationName] = {}
-    #                         try:
-    #                             orderTimeStamp = datetime.datetime.strptime(strippedLineList[1],"%Y%m%d%H%M")
-    #                         except:
-    #                             try:
-    #                                 orderTimeStamp = datetime.datetime.strptime(strippedLineList[1],"%Y%m%d")
-    #                             except:
-    #                                 orderTimeStamp = datetime.datetime.strptime(strippedLineList[1],"%Y%m")
-    #                         formattedOrderTimeStamp = orderTimeStamp.strftime("%Y-%m-%d %H:%M:%S")
-    #                         if formattedOrderTimeStamp not in dataToSave[stationName]:
-    #                             dataToSave[stationName][formattedOrderTimeStamp] = currentData[stationName]
-    #                         for param in dataToSave[stationName][formattedOrderTimeStamp]:
-    #                             if param in orderHeader:
-    #                                 index = orderHeader.index(param)
-    #                                 dataToSave[stationName][formattedOrderTimeStamp][param] = strippedLineList[index]
-    #             else:
-    #                 print('Wrong Format, make sure you chose "CSV" when downloading the file from IDAWEB')
-    #                 sys.exit(1)
-    #     data_order_file.close()
-    #     for key,value in dataToSave.items():
-    #         name = r''+key.replace("/",'\\')
-    #         temp = open(os.path.join(scraper_path,"temp-"+name+".csv"),"a+")
-    #         temp.write('localtime;'+header[name]+"\n")
-    #         str = ""
-    #         for ke,val in value.items():
-    #             str += ke+";"
-    #             for k,v in val.items():
-    #                 str += v+";"
-    #             str += "\n"
-    #         temp.write(str)
-    #         temp.close()
+
 def main(argv):
     exit_code = 0
     try:
