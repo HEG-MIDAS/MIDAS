9c977ce40a9dc66fc2cc1fc63f4755b18692ff29
Amir Alwash <amir.alwash@gmail.com>
2022-11-03 14:16:09 +0100

    Add print of ip and some infos

--- MIDAS/MIDAS/settings/base.py
+++ MIDAS/MIDAS/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'captcha',
+    'geoip2'
 ]
 
 MIDDLEWARE = [
@@ -150,3 +151,5 @@ CLIMACITY_ROOT = os.path.join(BASE_DIR, '../Climacity')
 SABRA_ROOT = os.path.join(BASE_DIR, '../SABRA')
 
 AUTH_USER_MODEL = 'MIDAS_app.User'
+
+GEOIP_PATH =os.path.join(BASE_DIR,'geoip')
