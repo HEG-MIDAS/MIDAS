7380c9a15ef13c0c8f6f4a0eb71173167a015f98
MagiPrince <david.nogueiras@outlook.com>
2022-12-08 12:38:20 +0100

    renaming file and repository and editing code

---
+++ VHG/vhg.py
@@ -0,0 +1,41 @@
+import time
+from hashlib import sha256
+from base64 import b64encode
+import requests
+import json
+# DO NOT FORGET TO CREATE env.py FILE WITH
+# env_username = with username
+# env_encrypted_password = with encrypted password
+from env import *
+
+server = "vhg.ch"
+page = "/io/web_service_json.php"
+
+dossier_id = 49001
+
+username = env_username
+encrypted_password = env_encrypted_password
+
+metering_code = "AU1"
+
+challenge_txt = str(int(time.time()))
+challenge_password = sha256((encrypted_password+challenge_txt).encode('utf-8')).hexdigest()
+
+url = "https://"+server+":"+str(443)+page
+
+headers = {
+    "Content-Type": "application/json",
+    "user_agent": "tetraedre/TDS",
+    "method": "POST",
+}
+data = {
+    "operation": "check_access",
+    "username": username,
+    "challenge": challenge_txt,
+    "dossier_id": dossier_id,
+    "challenge_password": challenge_password
+}
+
+r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
+print("------------------------Response------------------------")
+print(str(r)+"\n"+str(r.headers)+"\n"+str(r.content))
