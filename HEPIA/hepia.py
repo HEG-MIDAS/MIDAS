ccffc3f95eaa571974bdf0b2766cdaed7b9a2964
Amir Alwash <amir@alwash.ch>
2022-12-09 11:50:35 +0100

    Add hepia stuff

---
+++ HEPIA/hepia.py
@@ -0,0 +1,14 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+database = "midas"
+retention_policy = 'autogen'
+bucket = f'{database}/{retention_policy}'
+with InfluxDBClient.from_config_file("config.ini") as client:
+    print('*** Query Points ***')
+
+    query_api = client.query_api()
+    query = f'from(bucket: \"{bucket}\") |> range(start: -1h)'
+    tables = query_api.query(query)
+    for record in tables[0].records:
+        print(f'#{record.get_time()} #{record.get_measurement()}: #{record.get_field()} #{record.get_value()}')
