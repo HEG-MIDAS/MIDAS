e8958185387d8af23026c175b49184883b4d0438
Amir Alwash <amir.alwash@gmail.com>
2022-12-22 14:18:16 +0100

    add Sabra to jobrunner

--- docker/jobrunner.sh
+++ docker/jobrunner.sh
@@ -2,3 +2,6 @@
 echo "Starting Runner Job"  >> /proc/1/fd/1
 YDate=$(date -d "yesterday" +'%Y-%m-%d')
 python /app/Climacity/climacity.py -s $YDate -e $YDate >> /proc/1/fd/1
+echo "Sleeping for 60 seconds"  >> /proc/1/fd/1
+sleep 60
+python /app/SABRA/sabra.py -s $YDate -e $YDate >> /proc/1/fd/1
