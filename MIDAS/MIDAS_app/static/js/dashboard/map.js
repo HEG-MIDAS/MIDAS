50476762f8c39691b50ac8d2b1e78d531361cd61
MagiPrince <david.nogueiras@outlook.com>
2023-03-01 16:38:18 +0100

    working on the map interraction

--- MIDAS/MIDAS_app/static/js/dashboard/map.js
+++ MIDAS/MIDAS_app/static/js/dashboard/map.js
@@ -4,3 +4,38 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// L.Marker(
+//     [46.2026, 6.1235], {
+//     color: "red"
+//     }
+// ).addTo(map).on("click", circleClick);
+
+// function circleClick(e) {
+//     var clickedCircle = e.target;
+
+//     clickedCircle.bindPopup("some content").openPopup();
+// }
+
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var blueIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+var marker = new L.Marker([46.2026, 6.1235], {
+        icon: blueIcon
+        });
+marker.addTo(map);
